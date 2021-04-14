a021ee74381a6e4bc752a957c2c3d7e91f4d54ed
Andrii Hrushetskyi <hrushetskyi.andrii@gmail.com>
2021-04-14 19:29:11 -0400

    implemented plug API for get cheaper

---
+++ src/store/API/index.ts
@@ -0,0 +1,12 @@
+import { CategoryType } from "../../types/CategoryType";
+
+const BASE_URL = 'http://plug.plug';
+
+export const getCheaper = async (category: CategoryType) => {
+	const result = {
+		href: 'https://google.com',
+		price: 35,
+	}
+
+	return result;
+}
