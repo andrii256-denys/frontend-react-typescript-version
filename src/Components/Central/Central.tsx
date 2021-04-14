6db86feee9308509d8729b094f8ad1a9634da5e8
Andrii Hrushetskyi <hrushetskyi.andrii@gmail.com>
2021-04-14 19:30:07 -0400

    implemented Cetral with logic

--- src/Components/Central/Central.tsx
+++ src/Components/Central/Central.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { RadarPreloader } from '../RadarPreloader';
+import { useActions } from '../../hooks/useActions'
 
 import './Central.scss';
 
 export const Central: React.FunctionComponent = () => {
+	const { isError, isLoading, price, href } = useTypedSelector(state => state.goods.cheaper);
+	const { currentTab } = useTypedSelector(state => state.navigation);
+	const { fetchCheaper } = useActions();
+
+	useEffect(() => {
+		fetchCheaper(currentTab)
+	}, [currentTab]);
+
 	return (
 		<section className="Central">
 			<h1 className="Central__title">
 				Ціна {'найдешевшої гречки'} в Україні
 			</h1>
-			<p className={`Central__price-wrapper Central__price-wrapper--${'buckwheat'}`}>
+			<p className={`Central__price-wrapper Central__price-wrapper--${currentTab}`}>
 				<span className="Central__digits">
-					00
+					{isLoading && <RadarPreloader side={100} color="#aaa" />}
+					{isError && 'Error'}
+					{!isError && !isLoading && price}
 				</span>
 				<span className="Central__measure-units">грн / кг</span>
 			</p>
 			<a
-				href={'/'}
+				href={href}
 				target="_blank"
 				rel="noopener noreferrer"
-				className={`Central__link-to-seller Central__link-to-seller--${'buckwheat'}`}
+				className={`Central__link-to-seller Central__link-to-seller--${currentTab}`}
 			>
 				відкрити сайт продавця
 			</a>
