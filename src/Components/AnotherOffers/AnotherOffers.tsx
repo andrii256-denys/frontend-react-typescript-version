e5f7a7ebb6d136dd7993f29f67c2518e0b7f3dce
Andrii Hrushetskyi <hrushetskyi.andrii@gmail.com>
2021-04-18 13:47:53 -0400

    inserted FIlters into AnotherOffers

--- src/Components/AnotherOffers/AnotherOffers.tsx
+++ src/Components/AnotherOffers/AnotherOffers.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useActions } from '../../hooks/useActions';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { Filters } from '../Filters';
 
 import './AnotherOffers.scss';
 
 export const AnotherOffers: React.FunctionComponent = () => {
 	const { currentTab } = useTypedSelector(state => state.navigation)
+	const { isVisible: isFiltersVisible } = useTypedSelector(state => state.filters)
+	const { updateFiltersVisibility } = useActions();
 
 	return (
 		<article className={`restrictor1200 AnotherOffers AnotherOffers--${currentTab}`}>
@@ -12,16 +16,15 @@ export const AnotherOffers: React.FunctionComponent = () => {
 				<h2 className="AnotherOffers__title textChapterTitle">
 					Інші ціни
 				</h2>
-				PressfullButton
+				<button
+					className="AnotherOffers__filters-toggler"
+					onClick={() => { updateFiltersVisibility(isFiltersVisible ? 'hide' : 'show') }}
+				>
+					{isFiltersVisible ? 'Сховати' : 'Показати'} фільтри
+				</button>
 				<br></br>
 			</div>
-			<section className="AnotherOffers__filters">
-				price
-				<br />
-				sort by
-				<br></br>
-				shops
-			</section>
+			{isFiltersVisible && <Filters />}
 			<div className="AnotherOffers__bottom-row">
 				Products should be here
 			</div>
