ab7cda5aa1484fb96ae1d2db439afd2a94aac482
Andrii Hrushetskyi <hrushetskyi.andrii@gmail.com>
2021-04-20 08:58:35 -0400

    implemented Chart

--- src/Components/Chart/Chart.tsx
+++ src/Components/Chart/Chart.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
-import { LineChart, Line } from 'recharts'
+import classNames from 'classnames';
+import React, { useEffect } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { MONTHS_NAMES } from '../../constants/linguistics';
+import { useActions } from '../../hooks/useActions';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
 
-export const Chart: React.FunctionComponent = () => {
-	const data = [
-		{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-		{ name: 'Page B', uv: 300, pv: 2000, amt: 1600 },
-		{ name: 'Page C', uv: 600, pv: 1500, amt: 1750 },
-		{ name: 'Page D', uv: 200, pv: 3500, amt: 2800 },
-	];
+import './Chart.scss';
+
+const calcWidth = (): number => {
+	const viewport = document.body.offsetWidth;
+	let maxWidth: number;
+
+	if (viewport > 1260) {
+		maxWidth = 1200;
+	} else {
+		maxWidth = viewport - 60;
+	}
+
+	return maxWidth;
+}
+
+export const Chart = () => {
+	const { data: rawData, isLoading, isError } = useTypedSelector(state => state.statistics);
+	const { currentTab } = useTypedSelector(state => state.navigation);
+	const { fetchChartData } = useActions();
+
+	const data = rawData.map(item => ({
+		...item,
+		date: `${MONTHS_NAMES.casusNominativus[item.date.getMonth()]} ${item.date.getFullYear()}`,
+	}))
+
+	console.log(data)
+
+	useEffect(() => {
+		fetchChartData();
+	}, [currentTab])
+
 	return (
-		<LineChart width={400} height={400} data={data}>
-			<Line type="monotone" dataKey="uv" stroke="#8884d8" />
-		</LineChart>
+		<div className="Chart">
+			<div className={classNames('Chart__wrapper', { 'Chart__wrapper--hidden': isLoading || isError })}>
+				<LineChart
+					width={calcWidth()}
+					height={300}
+					data={data}
+				>
+					<XAxis dataKey="date" />
+					<YAxis />
+					<Tooltip />
+					<Line type="basis" dataKey="price" name="ціна" stroke="#542511" activeDot={{ r: 8 }} />
+				</LineChart>
+			</div>
+			{isLoading && (<div className="Chart__status-message Chart__status-message">Loading...</div>)}
+			{isError && (<div className="Chart__status-message Chart__status-message">Ooops, error ocured</div>)}
+		</div>
 	)
 }
